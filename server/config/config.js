81f7b9d3775ca4fb9ecc3741578fd5074380bf51
Saurabh Dabas <developerdabas@gmail.com>
2022-05-01 06:07:15 +0530

    Define server-side configuration variables

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3001,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: process.env.MONGO_PROD_URI
+  }
+  export default config ;
