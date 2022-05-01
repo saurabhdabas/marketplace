bac5978ff1ae0cc4be603d6e7abbf1733d27a757
Saurabh Dabas <developerdabas@gmail.com>
2022-05-01 07:44:36 +0530

    Connect the server to mongoDB using mongoose

--- server/app.js
+++ server/app.js
@@ -1,25 +1,34 @@
-const { MongoClient } = require('mongodb');
+const config = require('./config/config')
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
 // Connecting the server to mongoDB
 
-// MongoClient is the driver that connects to the running MongoDB instance using its URL. It allows us to implement the database-related code in the backend.
-const url = process.env.MONGODB_URI ||
-'mongodb://localhost:27017/mernSimpleSetup';
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true 
+  }
+)
+.then(() => console.log("Database connected!"))
+.catch(err => console.log(err));
 
-MongoClient.connect(url, (err, db)=>{
-console.log("Connected successfully to mongodb server")
-db.close()
-});
-
+app.use(cors());
+app.use(helmet());
 app.use(logger('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
