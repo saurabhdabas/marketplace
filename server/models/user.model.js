12ad8dba99cde8572d8645c151c7fbac22fdddca
Saurabh Dabas <developerdabas@gmail.com>
2022-05-02 10:02:54 +0530

    Add User Schema for a user

---
+++ server/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 'Email already exists',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email is required'
+  },
+  hashed_password: {
+    type: String,
+    required: "Password is required"
+  },
+  salt: String,
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('User', UserSchema);
